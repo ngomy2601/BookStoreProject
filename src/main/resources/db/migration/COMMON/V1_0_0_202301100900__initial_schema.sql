dfcca2ac2ce91fda0cb3fbb89f1759d67968aa83
ngomy2001 <ngomy2001@gmail.com>
2023-03-02 14:24:38 +0700

    created users table

--- src/main/resources/db/migration/COMMON/V1_0_0_202301100900__initial_schema.sql
+++ src/main/resources/db/migration/COMMON/V1_0_0_202301100900__initial_schema.sql
@@ -4,8 +4,21 @@ Create type role_name AS ENUM ('Admin', 'Contributor');
 
 CREATE TABLE roles
 (
-    id   UUID         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    id   uuid         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     name role_name
 );
 
+CREATE TABLE users
+(
+    id   uuid         NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
+    username VARCHAR(50) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    firstName VARCHAR(100),
+    lastName VARCHAR(100),
+    avatar bytea,
+    role_id uuid,
+    CONSTRAINT fk_roleId
+        FOREIGN KEY(role_id)
+            REFERENCES roles(id)
+);
 

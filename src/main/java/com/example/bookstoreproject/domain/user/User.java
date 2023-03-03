d689623beac82b4587aec454545ed89e391803df
ngomy2001 <ngomy2001@gmail.com>
2023-03-03 14:24:38 +0700

    Import Getter and Builder

--- src/main/java/com/example/bookstoreproject/domain/user/User.java
+++ src/main/java/com/example/bookstoreproject/domain/user/User.java
@@ -1,7 +1,12 @@
 package com.example.bookstoreproject.domain.user;
 
+import lombok.Builder;
+import lombok.Getter;
+
 import java.util.UUID;
 
+@Getter
+@Builder
 public class User {
     private UUID id;
     private String username;

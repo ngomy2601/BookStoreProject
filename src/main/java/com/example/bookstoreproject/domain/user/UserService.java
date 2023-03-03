9a6aee58c07381407987f9a2b712d513a6be340a
ngomy2001 <ngomy2001@gmail.com>
2023-03-03 14:25:02 +0700

    Created UserService in domain.user

---
+++ src/main/java/com/example/bookstoreproject/domain/user/UserService.java
@@ -0,0 +1,16 @@
+package com.example.bookstoreproject.domain.user;
+
+import com.example.bookstoreproject.persistence.user.UserStore;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserStore userStore;
+
+    public List<User> findAll() {
+        return userStore.findAll();
+    }
+}

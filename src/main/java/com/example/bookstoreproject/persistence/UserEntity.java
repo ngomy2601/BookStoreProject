7c9bb68c6f93b9e652551effde6433e5332cbef8
ngomy2001 <ngomy2001@gmail.com>
2023-03-03 13:39:37 +0700

    created UserEntity

---
+++ src/main/java/com/example/bookstoreproject/persistence/UserEntity.java
@@ -0,0 +1,26 @@
+package com.example.bookstoreproject.persistence;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Table;
+import java.util.UUID;
+
+@Table(name = "users")
+@Getter
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    private UUID id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private String role_id;
+
+}

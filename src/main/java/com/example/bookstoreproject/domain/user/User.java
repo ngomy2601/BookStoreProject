f43966f3945c6817b7f755308d1acf87f6572c13
ngomy2001 <ngomy2001@gmail.com>
2023-03-03 14:08:12 +0700

    created user in domain

---
+++ src/main/java/com/example/bookstoreproject/domain/user/User.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreproject.domain.user;
+
+import java.util.UUID;
+
+public class User {
+    private UUID id;
+    private String username;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String avatar;
+    private String role_id;
+}

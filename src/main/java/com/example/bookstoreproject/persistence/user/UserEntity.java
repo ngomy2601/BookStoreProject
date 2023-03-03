967d9a76cd54684cb977f1219b03d593e0a5d219
ngomy2001 <ngomy2001@gmail.com>
2023-03-03 15:53:29 +0700

    Add @Id, @Column in UserEntity.java

--- src/main/java/com/example/bookstoreproject/persistence/user/UserEntity.java
+++ src/main/java/com/example/bookstoreproject/persistence/user/UserEntity.java
@@ -5,6 +5,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 import java.util.UUID;
 
@@ -13,13 +16,18 @@ import java.util.UUID;
 @SuperBuilder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class UserEntity {
 
+    @Id
     private UUID id;
     private String username;
     private String password;
+    @Column(name = "firstname")
     private String firstName;
+    @Column(name = "lastname")
     private String lastName;
+
     private String avatar;
     private String role_id;
 

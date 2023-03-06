054273dce28e731948e9df90b6e6a56d6c762d26
ngomy2001 <ngomy2001@gmail.com>
2023-03-06 11:24:09 +0700

    Added @ResponseStatus

--- src/main/java/com/example/bookstoreproject/error/NotFoundException.java
+++ src/main/java/com/example/bookstoreproject/error/NotFoundException.java
@@ -1,7 +1,9 @@
 package com.example.bookstoreproject.error;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NotFoundException extends DomainException{
 
     public NotFoundException(final String message, Object... args){

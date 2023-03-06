a5b22fb5aa8c2594272c7324c8dffaae96a792fe
ngomy2001 <ngomy2001@gmail.com>
2023-03-06 09:58:53 +0700

    add annotation @With

--- src/main/java/com/example/bookstoreproject/domain/user/User.java
+++ src/main/java/com/example/bookstoreproject/domain/user/User.java
@@ -3,12 +3,14 @@ package com.example.bookstoreproject.domain.user;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.With;
 
 import java.util.UUID;
 
 @Getter
 @Setter
 @Builder
+@With
 public class User {
     private UUID id;
     private String username;

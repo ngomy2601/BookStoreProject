b376b761ca561f84792893b8580c876f1661aba6
ngomy2001 <ngomy2001@gmail.com>
2023-03-07 09:56:58 +0700

    added UserDTOMapperTest.java

---
+++ src/test/java/com/example/bookstoreproject/api/book/UserDTOMapperTest.java
@@ -0,0 +1,33 @@
+package com.example.bookstoreproject.api.book;
+
+import org.junit.jupiter.api.Test;
+
+import static com.example.bookstoreproject.api.book.UserDTOMapper.toUserDTO;
+import static com.example.bookstoreproject.api.book.UserDTOMapper.toUserDTOs;
+import static com.example.bookstoreproject.fakes.UserFakes.buildUser;
+import static com.example.bookstoreproject.fakes.UserFakes.buildUsers;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class UserDTOMapperTest {
+    @Test
+    void shouldToUserDTO_OK(){
+        final var user = buildUser();
+        final var userDTO = toUserDTO(user);
+
+        assertEquals(user.getId(), userDTO.getId());
+        assertEquals(user.getUsername(), userDTO.getUsername());
+        assertEquals(user.getPassword(), userDTO.getPassword());
+        assertEquals(user.getFirstName(), userDTO.getFirstName());
+        assertEquals(user.getLastName(), userDTO.getLastName());
+        assertEquals(user.getAvatar(), userDTO.getAvatar());
+        assertEquals(user.getRoleId(), userDTO.getRoleId());
+    }
+
+    @Test
+    void shouldToUserDTOs_OK(){
+        final var users = buildUsers();
+        final var userDTOs = toUserDTOs(users);
+        assertEquals(users.size(), userDTOs.size());
+    }
+
+}

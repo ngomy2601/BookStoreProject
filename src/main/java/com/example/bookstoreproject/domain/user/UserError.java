43160d1bc34bd80be1a030c19845ed03a4f2c2e0
ngomy2001 <ngomy2001@gmail.com>
2023-03-10 13:48:49 +0700

    Added supplyUsernameExist

--- src/main/java/com/example/bookstoreproject/domain/user/UserError.java
+++ src/main/java/com/example/bookstoreproject/domain/user/UserError.java
@@ -1,14 +1,18 @@
 package com.example.bookstoreproject.domain.user;
 
+import com.example.bookstoreproject.error.BadRequestException;
 import com.example.bookstoreproject.error.NotFoundException;
 import lombok.experimental.UtilityClass;
 
-import java.util.UUID;
 import java.util.function.Supplier;
 
 @UtilityClass
 public class UserError {
-    public static Supplier<NotFoundException> supplyUserNotFound(final UUID id) {
-        return () -> new NotFoundException("User with id %s could not be found", id);
+    public static <T> Supplier<NotFoundException> supplyUserNotFound(final T input) {
+        return () -> new NotFoundException("User with  %s could not be found", input);
+    }
+
+    public static Supplier<BadRequestException> supplyUsernameExisted(final String username) {
+        return () -> new BadRequestException("User with %s is existed", username);
     }
 }

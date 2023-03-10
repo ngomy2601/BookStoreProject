972384c97798928af663cfb42f351ae7c20cf35b
ngomy2001 <ngomy2001@gmail.com>
2023-03-10 09:33:22 +0700

    make it more consistent.

--- src/main/java/com/example/bookstoreproject/persistence/user/UserEntity.java
+++ src/main/java/com/example/bookstoreproject/persistence/user/UserEntity.java
@@ -22,13 +22,9 @@ public class UserEntity {
     private UUID id;
     private String username;
     private String password;
-
     private String firstName;
-
     private String lastName;
-
     private String avatar;
-
     private UUID roleId;
 
 }

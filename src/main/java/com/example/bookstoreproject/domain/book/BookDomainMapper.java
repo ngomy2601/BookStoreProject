25433ea2201d741e55aa1a647585486e8c38aa94
ngomy2001 <ngomy2001@gmail.com>
2023-03-13 16:22:54 +0700

    created BookDomainMapper.java

---
+++ src/main/java/com/example/bookstoreproject/domain/book/BookDomainMapper.java
@@ -0,0 +1,18 @@
+package com.example.bookstoreproject.domain.book;
+
+import com.example.bookstoreproject.api.book.BookDTO;
+
+public class BookDomainMapper {
+    public static Book toBook(final BookDTO bookDTO) {
+        return Book.builder()
+                .id(bookDTO.getId())
+                .title(bookDTO.getTitle())
+                .author(bookDTO.getAuthor())
+                .description(bookDTO.getDescription())
+                .createAt(bookDTO.getCreateAt())
+                .updateAt(bookDTO.getUpdateAt())
+                .image(bookDTO.getImage())
+                .userId(bookDTO.getUserId())
+                .build();
+    }
+}

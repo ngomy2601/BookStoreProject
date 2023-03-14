368e7f603343603b3cee4da020eb052aaafcb27e
ngomy2001 <ngomy2001@gmail.com>
2023-03-14 10:10:45 +0700

    created BookDTOMapper

---
+++ src/main/java/com/example/bookstoreproject/api/book/BookDTOMapper.java
@@ -0,0 +1,31 @@
+package com.example.bookstoreproject.api.book;
+
+import com.example.bookstoreproject.domain.book.Book;
+import lombok.experimental.UtilityClass;
+
+import java.util.List;
+
+import static org.apache.commons.collections4.ListUtils.emptyIfNull;
+
+@UtilityClass
+public class BookDTOMapper {
+    public static BookDTO toBookDTO(final Book book) {
+        return BookDTO.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .author(book.getAuthor())
+                .description(book.getDescription())
+                .createAt(book.getCreateAt())
+                .updateAt(book.getUpdateAt())
+                .image(book.getImage())
+                .userId(book.getUserId())
+                .build();
+    }
+
+    public static List<BookDTO> toBookDTOs(final List<Book> books) {
+        return emptyIfNull(books)
+                .stream()
+                .map(BookDTOMapper::toBookDTO)
+                .toList();
+    }
+}

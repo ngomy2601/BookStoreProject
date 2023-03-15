8d3ae65e6ba11bfbf8833e14711a362cd65f13df
ngomy2001 <ngomy2001@gmail.com>
2023-03-15 16:40:28 +0700

    added empty lines.

--- src/main/java/com/example/bookstoreproject/api/book/BookController.java
+++ src/main/java/com/example/bookstoreproject/api/book/BookController.java
@@ -15,23 +15,29 @@ import static com.example.bookstoreproject.domain.book.BookDomainMapper.toBook;
 @RequestMapping("/api/v1/books")
 @RequiredArgsConstructor
 public class BookController {
+
     private final BookService bookService;
+
     @GetMapping
     public List<BookDTO> findAll() {
         return toBookDTOs(bookService.findAll());
     }
+
     @GetMapping("{id}")
     public BookDTO findById(@PathVariable UUID id) {
         return toBookDTO(bookService.findById(id));
     }
+
     @PostMapping
     public BookDTO create(final @RequestBody BookDTO bookDTO) {
         return toBookDTO(bookService.create(toBook(bookDTO)));
     }
+
     @PutMapping("{id}")
     public BookDTO update(final @PathVariable UUID id, final @RequestBody BookDTO bookDTO) {
         return toBookDTO(bookService.update(id, toBook(bookDTO)));
     }
+    
     @DeleteMapping("{id}")
     public void delete(final @PathVariable UUID id) {
         bookService.delete(id);

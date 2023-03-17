9e04057ea4babf60a18969029e01d19238cc29a2
ngomy2001 <ngomy2001@gmail.com>
2023-03-17 14:45:47 +0700

    setup auth api

---
+++ src/main/java/com/example/bookstoreproject/api/auth/AuthController.java
@@ -0,0 +1,32 @@
+package com.example.bookstoreproject.api.auth;
+
+import com.example.bookstoreproject.domain.auths.JwtTokenService;
+import com.example.bookstoreproject.domain.auths.JwtUserDetails;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.example.bookstoreproject.api.auth.LoginDTOMapper.toAuthentication;
+
+@RestController
+@RequestMapping("api/v1/auths")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final JwtTokenService jwtTokenUtil;
+
+    private final AuthenticationManager authenticationManager;
+
+    @PostMapping
+    public JwtTokenResponseDTO login(@RequestBody LoginDTO loginDTO) {
+        final Authentication authentication = authenticationManager.authenticate(toAuthentication(loginDTO));
+
+        return JwtTokenResponseDTO.builder()
+                .token(jwtTokenUtil.generateToken((JwtUserDetails) authentication.getPrincipal()))
+                .build();
+    }
+}

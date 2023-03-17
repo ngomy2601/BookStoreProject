c1af7482e4339bad94a582dce98b6866048b7c16
ngomy2001 <ngomy2001@gmail.com>
2023-03-17 10:32:59 +0700

    added RoleStore

---
+++ src/main/java/com/example/bookstoreproject/persistence/role/RoleStore.java
@@ -0,0 +1,17 @@
+package com.example.bookstoreproject.persistence.role;
+
+import java.util.UUID;
+public class RoleStore {
+
+    public String getRoleById(final UUID uuid) {
+        if(uuid.equals(UUID.fromString("8720c2d6-f7d2-4ada-b0da-ee62a7640c6d"))) {
+            return "ROLE_ADMIN";
+        }
+
+        if(uuid.equals(UUID.fromString("7f146aeb-49cf-4e5c-ae27-7d8a290764d2"))) {
+            return "ROLE_USER";
+        }
+
+        return null;
+    }
+}

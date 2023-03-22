c79828c6e9324dcbf68442ffa7a0f12f6dd3f250
ngomy2001 <ngomy2001@gmail.com>
2023-03-22 14:26:44 +0700

    matched with real DB

--- src/main/java/com/example/bookstoreproject/persistence/role/RoleStore.java
+++ src/main/java/com/example/bookstoreproject/persistence/role/RoleStore.java
@@ -1,24 +1,19 @@
 package com.example.bookstoreproject.persistence.role;
 
+import com.example.bookstoreproject.domain.role.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
+
+import static com.example.bookstoreproject.persistence.role.RoleEntityMapper.toRole;
 
 @Repository
 @RequiredArgsConstructor
 public class RoleStore {
     private final RoleRepository roleRepository;
 
-    public String getRoleById(final UUID uuid) {
-        if (uuid.equals(UUID.fromString("8720c2d6-f7d2-4ada-b0da-ee62a7640c6d"))) {
-            return "ROLE_ADMIN";
-        }
-
-        if (uuid.equals(UUID.fromString("7f146aeb-49cf-4e5c-ae27-7d8a290764d2"))) {
-            return "ROLE_USER";
-        }
-
-        return null;
+    public Role findById(final UUID id) {
+        return toRole(roleRepository.findById(id).orElseThrow(null));
     }
 }

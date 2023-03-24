d03baaa5c1dda2ebfdc6ae86c9e597bb19ca16db
ngomy2001 <ngomy2001@gmail.com>
2023-03-24 10:03:54 +0700

    changed to Optional.

--- src/main/java/com/example/bookstoreproject/persistence/role/RoleStore.java
+++ src/main/java/com/example/bookstoreproject/persistence/role/RoleStore.java
@@ -4,16 +4,15 @@ import com.example.bookstoreproject.domain.role.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
-
-import static com.example.bookstoreproject.persistence.role.RoleEntityMapper.toRole;
 
 @Repository
 @RequiredArgsConstructor
 public class RoleStore {
     private final RoleRepository roleRepository;
 
-    public Role findById(final UUID id) {
-        return toRole(roleRepository.findById(id).orElseThrow(null));
+    public Optional<Role> findById(final UUID id) {
+        return roleRepository.findById(id).map(RoleEntityMapper::toRole);
     }
 }

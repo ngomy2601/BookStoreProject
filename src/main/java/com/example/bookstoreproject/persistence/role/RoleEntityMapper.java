72c8277ccb808f92ece89cbd16e210272d20ebae
ngomy2001 <ngomy2001@gmail.com>
2023-03-24 09:45:41 +0700

    removed toRoles and toRoleEntity

--- src/main/java/com/example/bookstoreproject/persistence/role/RoleEntityMapper.java
+++ src/main/java/com/example/bookstoreproject/persistence/role/RoleEntityMapper.java
@@ -3,10 +3,6 @@ package com.example.bookstoreproject.persistence.role;
 import com.example.bookstoreproject.domain.role.Role;
 import lombok.experimental.UtilityClass;
 
-import java.util.List;
-
-import static org.apache.commons.collections4.ListUtils.emptyIfNull;
-
 @UtilityClass
 public class RoleEntityMapper {
 
@@ -14,20 +10,6 @@ public class RoleEntityMapper {
         return Role.builder()
                 .id(roleEntity.getId())
                 .name(roleEntity.getName())
-                .build();
-    }
-
-    public static List<Role> toRoles(final List<RoleEntity> roleEntities) {
-        return emptyIfNull(roleEntities)
-                .stream()
-                .map(RoleEntityMapper::toRole)
-                .toList();
-    }
-
-    public static RoleEntity toRoleEntity(final Role role) {
-        return RoleEntity.builder()
-                .id(role.getId())
-                .name(role.getName())
                 .build();
     }
 }

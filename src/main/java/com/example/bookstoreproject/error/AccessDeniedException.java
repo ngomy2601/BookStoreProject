b971b2476ecd835cfe978aa31ea39066970a478a
ngomy2001 <ngomy2001@gmail.com>
2023-03-24 10:59:15 +0700

    created AccessDeniedException.java

---
+++ src/main/java/com/example/bookstoreproject/error/AccessDeniedException.java
@@ -0,0 +1,8 @@
+package com.example.bookstoreproject.error;
+
+import org.springframework.http.HttpStatus;
+
+public class AccessDeniedException extends DomainException{
+
+    public AccessDeniedException(String message, Object... args) { super(HttpStatus.FORBIDDEN, message, args);}
+}

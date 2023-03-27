d45e97b518eecfdc952bc149abad477d894b6376
Vo Quang Hoa <hvquang@openwt.com>
2023-03-27 14:27:07 +0700

    Fix build warning

--- src/main/java/com/example/bookstoreproject/properties/JwtProperties.java
+++ src/main/java/com/example/bookstoreproject/properties/JwtProperties.java
@@ -2,17 +2,18 @@ package com.example.bookstoreproject.properties;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
 
 @Component
 @Getter
 @Setter
 @ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
+
     @NotBlank
     private String secret;
 

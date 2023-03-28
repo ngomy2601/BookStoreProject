d2964a3020117238e9185fc1ce5db72b6eac03cc
ngomy2001 <ngomy2001@gmail.com>
2023-03-28 12:11:34 +0700

    removed ActiveProfiles

--- src/test/java/com/example/bookstoreproject/BookStoreProjectApplicationTests.java
+++ src/test/java/com/example/bookstoreproject/BookStoreProjectApplicationTests.java
@@ -2,10 +2,8 @@ package com.example.bookstoreproject;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("TEST")
 class BookStoreProjectApplicationTests {
 
     @Test

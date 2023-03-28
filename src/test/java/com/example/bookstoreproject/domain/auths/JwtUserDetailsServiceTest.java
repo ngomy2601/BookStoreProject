e77cd5145b6bba81c7fb9aea96efc33cd464a210
ngomy2001 <ngomy2001@gmail.com>
2023-03-28 14:59:19 +0700

    tested loadUserByUsername_WhenUsernameIsNotFound_OK

--- src/test/java/com/example/bookstoreproject/domain/auths/JwtUserDetailsServiceTest.java
+++ src/test/java/com/example/bookstoreproject/domain/auths/JwtUserDetailsServiceTest.java
@@ -3,19 +3,26 @@ package com.example.bookstoreproject.domain.auths;
 import com.example.bookstoreproject.error.NotFoundException;
 import com.example.bookstoreproject.persistence.role.RoleStore;
 import com.example.bookstoreproject.persistence.user.UserStore;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Optional;
 
+import static com.example.bookstoreproject.fakes.RoleFakes.buildRole;
+import static com.example.bookstoreproject.fakes.UserFakes.buildUser;
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(SpringExtension.class)
 class JwtUserDetailsServiceTest {
 
     @Mock
@@ -26,15 +33,27 @@ class JwtUserDetailsServiceTest {
     @InjectMocks
     private JwtUserDetailsService jwtUserDetailsService;
 
-    @BeforeEach
-    public void setUp() {
-        openMocks(this);
-        jwtUserDetailsService = new JwtUserDetailsService(userStore, roleStore);
-    }
     @Test
     void loadUserByUsername() {
         final var username = randomAlphabetic(3, 10);
         when(userStore.findByUsername(any())).thenReturn(Optional.empty());
         assertThrows(NotFoundException.class, () -> jwtUserDetailsService.loadUserByUsername(username));
+    }
+
+    @Test
+    void loadUserByUsername_WhenUsernameIsNotFound_OK() {
+        final var user = buildUser();
+        final var role = buildRole();
+
+        role.setId(user.getRoleId());
+        when(userStore.findByUsername(any())).thenReturn(Optional.of(user));
+
+        when(roleStore.findById(role.getId())).thenReturn(Optional.of(role));
+
+        final UserDetails actual = jwtUserDetailsService.loadUserByUsername(user.getUsername());
+
+        assertEquals(user.getUsername(), actual.getUsername());
+        verify(userStore).findByUsername(anyString());
+        verify(roleStore).findById(role.getId());
     }
 }

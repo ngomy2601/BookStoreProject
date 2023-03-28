1b03e958600ee9122f4542e447d2d8cbd4aa9979
ngomy2001 <ngomy2001@gmail.com>
2023-03-28 14:50:22 +0700

    removed beforeEach

--- src/test/java/com/example/bookstoreproject/domain/auths/JwtTokenServiceTest.java
+++ src/test/java/com/example/bookstoreproject/domain/auths/JwtTokenServiceTest.java
@@ -3,10 +3,12 @@ package com.example.bookstoreproject.domain.auths;
 import com.example.bookstoreproject.properties.JwtProperties;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.util.Collections;
 import java.util.Date;
@@ -15,27 +17,23 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
-import static org.mockito.MockitoAnnotations.openMocks;
 
+@ExtendWith(SpringExtension.class)
 class JwtTokenServiceTest {
 
     private static final String SECRET = "testSecret";
     private static final Long EXPIRATION = 3600L;
+
+    @InjectMocks
     private JwtTokenService jwtTokenService;
     @Mock
     private JwtProperties jwtProperties;
 
-    @BeforeEach
-    void setUp() {
-        openMocks(this);
-        jwtTokenService = new JwtTokenService(jwtProperties);
-    }
     @Test
     void generateToken_OK() {
         final JwtUserDetails jwtUserDetails = new JwtUserDetails(null, "user", randomAlphabetic(3, 10), Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
         when(jwtProperties.getSecret()).thenReturn(SECRET);
         when(jwtProperties.getExpiration()).thenReturn(EXPIRATION);
-
         final String token = jwtTokenService.generateToken(jwtUserDetails);
         final Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
         final Date expiration = claims.getExpiration();

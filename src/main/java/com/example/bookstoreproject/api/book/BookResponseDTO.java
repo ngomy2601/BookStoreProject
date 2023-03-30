b68ff666786366d36612325f6577b906ad659050
ngomy2001 <ngomy2001@gmail.com>
2023-03-30 14:25:18 +0700

    created BookResponseDTO and BookResponseDTOMapper

---
+++ src/main/java/com/example/bookstoreproject/api/book/BookResponseDTO.java
@@ -0,0 +1,20 @@
+package com.example.bookstoreproject.api.book;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Getter
+@Builder
+public class BookResponseDTO {
+    private UUID id;
+    private String title;
+    private String author;
+    private String description;
+    private Instant createAt;
+    private Instant updateAt;
+    private String image;
+    private UUID userId;
+}

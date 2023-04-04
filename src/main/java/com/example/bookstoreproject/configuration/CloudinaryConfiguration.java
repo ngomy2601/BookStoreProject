38d42db99f6ebc1d9228ea45f474adf38a437176
ngomy2001 <ngomy2001@gmail.com>
2023-04-04 09:46:56 +0700

    refactor CloudinaryConfiguration.java

--- src/main/java/com/example/bookstoreproject/configuration/CloudinaryConfiguration.java
+++ src/main/java/com/example/bookstoreproject/configuration/CloudinaryConfiguration.java
@@ -1,30 +1,30 @@
 package com.example.bookstoreproject.configuration;
 
 import com.cloudinary.Cloudinary;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
-@PropertySource(value = {"classpath:application-dev.properties"})
+@ConfigurationProperties(prefix = "cloudinary")
+
 public class CloudinaryConfiguration {
-    @Value("${cloudinary.name}")
-    private String cloudinaryName;
-    @Value("${cloudinary.secret}")
-    private String cloudinarySecret;
-    @Value("${cloudinary.key}")
-    private String cloudinaryKey;
+
+    private String name;
+
+    private String secret;
+
+    private String key;
 
     @Bean
     public Cloudinary cloudinary() {
-        Map<String, String> config = new HashMap<>();
-        config.put("cloud_name", cloudinaryName);
-        config.put("api_key", cloudinaryKey);
-        config.put("api_secret", cloudinarySecret);
+        final Map<String, String> config = new HashMap<>();
+        config.put("cloud_name", name);
+        config.put("api_key", key);
+        config.put("api_secret", secret);
         return new Cloudinary(config);
     }
 }

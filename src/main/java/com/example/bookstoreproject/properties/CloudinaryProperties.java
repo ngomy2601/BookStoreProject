f4957e35df2ba2e4cdc2930af48225b228756c7c
ngomy2001 <ngomy2001@gmail.com>
2023-04-07 11:28:54 +0700

    created CloudinaryProperties.java

---
+++ src/main/java/com/example/bookstoreproject/properties/CloudinaryProperties.java
@@ -0,0 +1,16 @@
+package com.example.bookstoreproject.properties;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "cloudinary")
+public class CloudinaryProperties {
+    private String name;
+    private String key;
+    private String secret;
+}

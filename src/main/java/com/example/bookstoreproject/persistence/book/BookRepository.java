6ab858749b25137d8de681b454e63367f99613f4
ngomy2001 <ngomy2001@gmail.com>
2023-04-10 10:17:54 +0700

    added find()

--- src/main/java/com/example/bookstoreproject/persistence/book/BookRepository.java
+++ src/main/java/com/example/bookstoreproject/persistence/book/BookRepository.java
@@ -3,12 +3,15 @@ package com.example.bookstoreproject.persistence.book;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface BookRepository extends CrudRepository<BookEntity, UUID> {
     Optional<BookEntity> findByTitle(final String title);
+
+    List<BookEntity> find(final String keyword);
 
     Optional<BookEntity> findByIsbn13(final String isbn13);
 }

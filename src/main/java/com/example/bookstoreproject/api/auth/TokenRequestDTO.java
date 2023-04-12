17ea379df5b304be9f9cc8de089d21d8470d2346
ngomy2001 <ngomy2001@gmail.com>
2023-04-12 14:31:47 +0700

    created TokenRequestDTO

---
+++ src/main/java/com/example/bookstoreproject/api/auth/TokenRequestDTO.java
@@ -0,0 +1,13 @@
+package com.example.bookstoreproject.api.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TokenRequestDTO {
+
+    private String idToken;
+}

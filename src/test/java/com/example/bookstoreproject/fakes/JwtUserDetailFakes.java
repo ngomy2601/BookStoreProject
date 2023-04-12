8b71bedb524c5cb63f485dccbd43b867624f63fe
ngomy2001 <ngomy2001@gmail.com>
2023-04-12 16:25:05 +0700

    created JwtUserDetailFakes

---
+++ src/test/java/com/example/bookstoreproject/fakes/JwtUserDetailFakes.java
@@ -0,0 +1,16 @@
+package com.example.bookstoreproject.fakes;
+
+import com.example.bookstoreproject.domain.auths.JwtUserDetails;
+import lombok.experimental.UtilityClass;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collections;
+
+import static java.util.UUID.randomUUID;
+
+@UtilityClass
+public class JwtUserDetailFakes {
+    public static JwtUserDetails buildJwtUserDetails() {
+        return new JwtUserDetails(randomUUID(), "user", "123123", Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}

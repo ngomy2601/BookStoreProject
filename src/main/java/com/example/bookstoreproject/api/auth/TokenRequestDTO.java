e1043904d7eb134ce55360e12e2ceda187c0f44d
ngomy2001 <ngomy2001@gmail.com>
2023-04-13 15:44:10 +0700

    added TokenRequestDTO

---
+++ src/main/java/com/example/bookstoreproject/api/auth/TokenRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.bookstoreproject.api.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TokenRequestDTO {
+
+    private String accessToken;
+}

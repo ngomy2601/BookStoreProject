d18995377421317e3988924f771c01601551fd7e
ngomy2001 <ngomy2001@gmail.com>
2023-04-13 15:21:04 +0700

    created SocialUser

---
+++ src/main/java/com/example/bookstoreproject/domain/user/SocialUser.java
@@ -0,0 +1,15 @@
+package com.example.bookstoreproject.domain.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Builder
+@Getter
+@Setter
+public class SocialUser {
+
+    private String firstName;
+    private String lastName;
+    private String username;
+}

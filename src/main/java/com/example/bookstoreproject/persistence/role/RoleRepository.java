6ea0003160322d6086047873c6446848de2c203e
ngomy2001 <ngomy2001@gmail.com>
2023-04-13 15:08:14 +0700

    created RoleRepository

--- src/main/java/com/example/bookstoreproject/persistence/role/RoleRepository.java
+++ src/main/java/com/example/bookstoreproject/persistence/role/RoleRepository.java
@@ -2,8 +2,9 @@ package com.example.bookstoreproject.persistence.role;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface RoleRepository extends CrudRepository<RoleEntity, UUID> {
-
+    Optional<RoleEntity> findByName(final String name);
 }

345b170d0f9ff5982959449b561f7625e9aa7421
ngomy2001 <ngomy2001@gmail.com>
2023-04-13 15:58:39 +0700

    fixed contextLoads()

--- src/test/java/com/example/bookstoreproject/BookStoreProjectApplicationTests.java
+++ src/test/java/com/example/bookstoreproject/BookStoreProjectApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.bookstoreproject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = BookStoreProjectApplicationTests.class)
 class BookStoreProjectApplicationTests {
 
     @Test

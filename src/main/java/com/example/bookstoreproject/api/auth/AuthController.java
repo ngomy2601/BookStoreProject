0a54d8981323378e2d182cbae1c0090e890c3935
ngomy2001 <ngomy2001@gmail.com>
2023-04-13 15:46:38 +0700

    added loginWithFacebook()

--- src/main/java/com/example/bookstoreproject/api/auth/AuthController.java
+++ src/main/java/com/example/bookstoreproject/api/auth/AuthController.java
@@ -2,11 +2,13 @@ package com.example.bookstoreproject.api.auth;
 
 import com.example.bookstoreproject.domain.auths.JwtTokenService;
 import com.example.bookstoreproject.domain.auths.JwtUserDetails;
+import com.example.bookstoreproject.domain.user.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InternalAuthenticationServiceException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,6 +26,8 @@ public class AuthController {
 
     private final AuthenticationManager authenticationManager;
 
+    private final UserService userService;
+
     @PostMapping
     public JwtTokenResponseDTO login(final @RequestBody LoginDTO loginDTO) throws Exception {
         try {
@@ -35,5 +39,18 @@ public class AuthController {
         } catch (BadCredentialsException | InternalAuthenticationServiceException e) {
             throw supplyUnauthorizedError().get();
         }
+    }
+
+    @PostMapping("/facebook")
+    public JwtTokenResponseDTO loginWithFacebook(final @RequestBody TokenRequestDTO tokenRequestDTO) {
+        final UserDetails userDetails = userService.loginWithFacebook(tokenRequestDTO.getAccessToken());
+
+        return generateJwtToken((JwtUserDetails) userDetails);
+    }
+
+    private JwtTokenResponseDTO generateJwtToken(final JwtUserDetails userDetails) {
+        return JwtTokenResponseDTO.builder()
+                .token(jwtTokenService.generateToken(userDetails))
+                .build();
     }
 }

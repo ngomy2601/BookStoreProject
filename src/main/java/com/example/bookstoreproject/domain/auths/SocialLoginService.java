7d0f0a950f31e16dd60bbb5da86aeb90fec025fa
ngomy2001 <ngomy2001@gmail.com>
2023-04-14 14:54:14 +0700

    added loginFacebook()

--- src/main/java/com/example/bookstoreproject/domain/auths/SocialLoginService.java
+++ src/main/java/com/example/bookstoreproject/domain/auths/SocialLoginService.java
@@ -16,18 +16,27 @@ import static java.util.Collections.singleton;
 
 @Service
 @RequiredArgsConstructor
-public class GoogleLoginService {
+public class SocialLoginService {
     private final GoogleTokenVerifierService googleTokenVerifierService;
+
+    private final FacebookTokenVerifierService facebookTokenVerifierService;
 
     private final UserStore userStore;
 
     private final RoleStore roleStore;
 
     public UserDetails loginGoogle(final String decodedToken) {
-        final GoogleTokenPayload googleAccount = googleTokenVerifierService.googleIdTokenVerifier(decodedToken);
+        final SocialTokenPayload googleAccount = googleTokenVerifierService.googleIdTokenVerifier(decodedToken);
         return userStore.findByUsername(googleAccount.getEmail())
                 .map(this::getJwtUserDetails)
-                .orElseGet(() -> createNewGoogleUser(googleAccount));
+                .orElseGet(() -> createNewSocialUser(googleAccount, "google"));
+    }
+
+    public UserDetails loginFacebook(final String decodeToken) {
+        final var facebookAccount = facebookTokenVerifierService.verifyFacebookToken(decodeToken);
+        return userStore.findByUsername(facebookAccount.getUsername())
+                .map(this::getJwtUserDetails)
+                .orElseGet(() -> createNewSocialUser(facebookAccount, "facebook"));
     }
 
     private JwtUserDetails getJwtUserDetails(final User user) {
@@ -35,18 +44,26 @@ public class GoogleLoginService {
         return new JwtUserDetails(user.getId(), user.getUsername(), user.getPassword(), createAuthorities(roleName));
     }
 
-    private JwtUserDetails createNewGoogleUser(final GoogleTokenPayload googleAccount) {
+
+    private JwtUserDetails createNewSocialUser(final SocialTokenPayload socialAccount, final String typeAccount) {
         final UUID roleId = roleStore.findIdByName("CONTRIBUTOR");
-        final User newUser = createUser(googleAccount, roleId);
+        final User newUser = createUser(socialAccount, roleId, typeAccount);
         return new JwtUserDetails(newUser.getId(), newUser.getUsername(), newUser.getPassword(), createAuthorities("CONTRIBUTOR"));
     }
 
-    private User createUser(final GoogleTokenPayload googleAccount, UUID roleId) {
+    private User createUser(final SocialTokenPayload socialAccount, final UUID roleId, final String typeAccount) {
         final User newUser = User.builder()
-                .username(googleAccount.getEmail())
                 .password(UUID.randomUUID().toString())
+                .firstName(socialAccount.getFirstName())
+                .lastName(socialAccount.getLastName())
                 .roleId(roleId)
                 .build();
+
+        if (typeAccount.equals("google")) {
+            newUser.setUsername(socialAccount.getEmail());
+        } else if (typeAccount.equals("facebook")) {
+            newUser.setUsername(socialAccount.getUsername());
+        }
         return userStore.create(newUser);
     }
 

937290cfe7c5f58474123591a96d63e212ebe341
ngomy2001 <ngomy2001@gmail.com>
2023-04-14 14:53:39 +0700

    changed path

--- src/test/java/com/example/bookstoreproject/api/auth/HomeControllerTest.java
+++ src/test/java/com/example/bookstoreproject/api/auth/HomeControllerTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @WebMvcTest(HomeController.class)
 class HomeControllerTest extends AbstractControllerTest {
@@ -14,8 +13,7 @@ class HomeControllerTest extends AbstractControllerTest {
     @Test
     @WithMockAdmin
     public void shouldLoginPage_OK() throws Exception {
-        get("/google")
-                .andExpect(status().isOk())
-                .andExpect(view().name("index.html"));
+        get("/login")
+                .andExpect(status().isOk());
     }
 }
